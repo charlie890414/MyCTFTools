08171eeef346d9a1d07f0d424472da6966d16005
charlie890414 <charlie890414@gmail.com>
2020-01-06 00:37:31 +0800

    update .travis.yml

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@ pip install angr pwntools
 
 gem install one_gadget
 
-useradd -m ctf && echo "ctf ALL=NOPASSWD: ALL" > /etc/sudoers.d/ctf
-
 git clone https://github.com/scwuaptx/peda.git ~/peda && echo "source ~/peda/peda.py" >> ~/.gdbinit && cp ~/peda/.inputrc ~/ 
 
 git clone https://github.com/scwuaptx/Pwngdb.git ~/Pwngdb && cp ~/Pwngdb/.gdbinit ~/
